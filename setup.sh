9a3f955eee05d24b3cae685e037d6d74b14e6a01
Antonin Lindner <lantonin@cern.ch>
2024-04-05 21:57:11 +0100

    removed VENV activation and python scripts sourcing

--- setup.sh
+++ setup.sh
@@ -4,7 +4,6 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 BIN_DIR="$SCRIPT_DIR/bin/"
-PY_DIR="$SCRIPT_DIR/scripts/"
 
 if [ -d "$BIN_DIR" ]; then
   # Add the directory to the PATH if it exists
@@ -13,18 +12,3 @@ if [ -d "$BIN_DIR" ]; then
 else
   echo "Error: $BIN_DIR does not exist."
 fi
-
-if [ -d "$PY_DIR" ]; then
-  # Add the directory to the PATH if it exists
-  export PATH="$PY_DIR:$PATH"
-  echo "Added $PY_DIR to PATH"
-else
-  echo "Error: $PY_DIR does not exist."
-fi
-
-# activate Python venv environment that meets INFN electronics dependencies
-. ~/.venvs/PythonINFN/bin/activate
-
-# mb check if environment exists and create it if not :) ... makefile thing
-# pip install argparse os time sys cmd2 functools getpass numpy
-# struct minimalmodbus==1.0.2
